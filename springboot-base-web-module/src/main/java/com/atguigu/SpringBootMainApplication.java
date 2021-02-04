20c92738a1bd7d58a2ba71e179cb51b7f4e09368
samsin-xll <1040118606@qq.com>
2021-02-04 22:45:02 +0800

    修改文件注释信息。

--- springboot-base-web-module/src/main/java/com/atguigu/SpringBootMainApplication.java
+++ springboot-base-web-module/src/main/java/com/atguigu/SpringBootMainApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 说明：SpringBoot应用启动类
- *
  * @author：李尚兴
  * @date：2021-02-03 18:47
  * @version：1.0
  */
-@SpringBootApplication
+@SpringBootApplication // 标记SpringBoot应用
 public class SpringBootMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMainApplication.class, args); // 启动Spring应用
