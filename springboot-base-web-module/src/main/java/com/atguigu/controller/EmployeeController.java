52b78dbbceb9fa514165203f18fec38ab320d53e
lishangxing <lishangxing@szboanda.net>
2021-03-05 21:55:06 +0800

    完成springboot增删改查功能。

--- springboot-base-web-module/src/main/java/com/atguigu/controller/EmployeeController.java
+++ springboot-base-web-module/src/main/java/com/atguigu/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import com.atguigu.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -23,6 +23,50 @@ public class EmployeeController {
     private EmployeeDao employeeDao;
     @Autowired
     private DepartmentDao departmentDao;
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id) {
+        System.out.println(id);
+        this.employeeDao.delete(id);
+        return "redirect:/emps";
+    }
+
+    /**
+     * 修改员工
+     * @param employee 员工对象
+     * @return
+     */
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee) {
+        this.employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    /**
+     * 跳转到员工修改页面
+     * @param id 员工id
+     * @param model 封装值对象
+     * @return
+     */
+    @GetMapping("/emp/{id}")
+    public String toEditEmpPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = this.employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        Collection<Department> departments = this.departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    /**
+     * 添加员工
+     * @param employee 员工对象
+     * @return
+     */
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        this.employeeDao.save(employee);
+        return "redirect:/emps";
+    }
 
     /**
      * 跳转到员工添加页面
